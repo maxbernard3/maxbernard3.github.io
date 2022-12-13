c062c486dfe982f12ea88b2b679f95a5dc26b6bd
BERNARD Max <80251657+maxbernard3@users.noreply.github.com>
2022-12-13 18:54:32 +0100

    q

--- js/responsive.js
+++ js/responsive.js
@@ -1,6 +1,5 @@
 console.log("is loaded")
 var loca = document.getElementById("location")
-var vk = false
 
 loca.ontouchstart = function () {
     document.getElementsByClassName("header")[0].style.display = "none";
@@ -9,16 +8,13 @@ loca.ontouchstart = function () {
     //document.getElementsByClassName("container_santa")[0].style.justifyContent = "start";
     //document.getElementsByClassName("inputs")[0].style.marginTop = "2vh";
     //loca.focus();
-    A = true;
 }
 
 window.onresize = function () {
-    if(vk){
-        document.getElementsByClassName("header")[0].style.display = "flex";
-        document.getElementById("calendar_btn").style.display ="flex";
-        document.getElementsByClassName("footer")[0].style.display = "flex";
-        document.getElementsByClassName("container_santa")[0].style.justifyContent = "center";
-    } 
+    document.getElementsByClassName("header")[0].style.display = "flex";
+    document.getElementById("calendar_btn").style.display ="flex";
+    document.getElementsByClassName("footer")[0].style.display = "flex";
+    //document.getElementsByClassName("container_santa")[0].style.justifyContent = "center";
 }
 
 function sleep(ms) {
