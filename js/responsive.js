b796435ced268e696f02eb0af81c3f84cb1b143d
BERNARD Max <80251657+maxbernard3@users.noreply.github.com>
2022-12-13 18:07:57 +0100

    test

--- js/responsive.js
+++ js/responsive.js
@@ -1,19 +1,30 @@
 console.log("is loaded")
 var loca = document.getElementById("location")
+var vk = false
+
 loca.ontouchstart = function () {
     document.getElementsByClassName("header")[0].style.display = "none";
     document.getElementsByClassName("footer")[0].style.display = "none";
     document.getElementById("calendar_btn").style.display = "none";
     document.getElementsByClassName("container_santa")[0].style.justifyContent = "start";
-    document.getElementsByClassName("inputs")[0].style.marginTop = "2vh"
-    loca.select();
+    document.getElementsByClassName("inputs")[0].style.marginTop = "2vh";
+    
+    sleep(200)
 
-    sleep(500)
+    loca.focus();
+    console.log("debug");
+    A = true;
 }
 
 window.onresize = function () {
-    document.getElementsByClassName("header")[0].style.display = "flex";
-    document.getElementById("calendar_btn").style.display ="flex";
-    document.getElementsByClassName("footer")[0].style.display = "flex";
-    document.getElementsByClassName("container_santa")[0].style.justifyContent = "center";
+    if(vk){
+        document.getElementsByClassName("header")[0].style.display = "flex";
+        document.getElementById("calendar_btn").style.display ="flex";
+        document.getElementsByClassName("footer")[0].style.display = "flex";
+        document.getElementsByClassName("container_santa")[0].style.justifyContent = "center";
+    } 
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
