03e99f439b3b7dacc080c2f8b38b5fa2d50fa28c
BERNARD Max <80251657+maxbernard3@users.noreply.github.com>
2022-12-13 17:49:50 +0100

    Update responsive.js

--- js/responsive.js
+++ js/responsive.js
@@ -1,12 +1,12 @@
 console.log("is loaded")
 var loca = document.getElementById("location")
 loca.ontouchstart = function () {
+    loca.select();
     document.getElementsByClassName("header")[0].style.display = "none";
     document.getElementsByClassName("footer")[0].style.display = "none";
     document.getElementById("calendar_btn").style.display = "none";
     document.getElementsByClassName("container_santa")[0].style.justifyContent = "start";
     document.getElementsByClassName("inputs")[0].style.marginTop = "2vh"
-    //loca.select();
 }
 
 window.onresize = function () {
